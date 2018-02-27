2748c723ff0e69b5ba5fd744a443500f0edb258a
PHJSmith <peterhjsmith1999@gmail.com>
2018-02-27 16:52:45 +0000

    small changes

--- resources/views/restrictions/index.blade.php
+++ resources/views/restrictions/index.blade.php
@@ -18,8 +18,8 @@
   <p>Each planet has been ranked according to planetary size, population, proximity to potential threats and strategic significance to the Union as a whole.</p>
   <ul>
     <li><strong>Class A:</strong> These planets will be permitted a sizeable planetside force, planetary and orbital weapons, and a small spacefleet.</li>
-    <li><strong>Class B:</strong> These planets will be permitted a planetside force, planetary and orbital weapons, but no spacefleet.</li>
-    <li><strong>Class C:</strong> These planets will be able to field planetside forces as well planetary guns and shields, but no spacecraft.</li>
+    <li><strong>Class B:</strong> These planets will be permitted a planetside force, planetary and orbital weapons, but no warships.</li>
+    <li><strong>Class C:</strong> These planets will be able to field planetside forces as well planetary guns and shields, but no armed spacecraft.</li>
     <li><strong>Class D:</strong> Planets of this class may field shields and small arms troops, as well as a small planetside force.</li>
     <li><strong>Class Null:</strong> Any planet which abuses the trust of the Authority and rebels against, threatens or undermines the Mercian Authority will be relegated to this class. Planets that assault other Union planets or field militaries not permitted by the Authority will also be relegated. This relegation will always be temporary, although the length of relegation can vary. Planets of this class may not field any military of any kind, bar small arms infantry and police. The planet will be assigned a Corpus Militaris detachment to protect it.</li>
     <li><strong>Class AA:</strong> In the event that a planet becomes involved in a warzone or is otherwise threatened by forces opposing the Mercian Authority, emergency measures will be taken and the planet will not be denied a military of sufficient strength to protect its people. The IDM restrictions will be lessened or removed entirely until the threat is removed.</p>
